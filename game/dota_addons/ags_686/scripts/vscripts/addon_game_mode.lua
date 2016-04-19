bb31a466a81226b8e6400da741c58105c9fa4cf8
Tianren Li <litianren01@qq.com>
2016-04-19 01:21:02 -0500

    160418 bug fixed

--- game/dota_addons/ags_686/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/ags_686/scripts/vscripts/addon_game_mode.lua
@@ -258,7 +258,12 @@ function CagsGameMode:AbandonCheckThink()
 								PlayerResource:ModifyGold(j,math.floor((PlayerResource:GetGold(j)+ItemCost)*(GCMulti-1)), false, 0)
 							end
 						end				
-						PlayerResource:ModifyGold(i,math.floor(ItemCosti/2), false, 0)
+						for k = 0, 11 do
+							if PlayerResource:GetSelectedHeroEntity(i):GetItemInSlot(k) then
+								PlayerResource:GetSelectedHeroEntity(i):RemoveItem(PlayerResource:GetSelectedHeroEntity(i):GetItemInSlot(k))
+							end
+						end									
+						PlayerResource:ModifyGold(i,math.floor(ItemCosti), false, 0)
 						--GameRules:SendCustomMessage("%s1 abandoned. All dire players' gold, GPM and respawn speed "..((math.floor(DireGC*100))/100).."x.", i, 0)				
 	  				Notifications:BottomToAll({hero=PlayerResource:GetSelectedHeroName(i), imagestyle="landscape", duration=10.0})
 	  				Notifications:BottomToAll({text="#addon_abandon_dire_01", continue=true, style={["font-size"]="30px"}})
